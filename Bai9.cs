2c144cd235e556f0d3921c9e1142afb78e387fa1
nghailong124-afk <nghailong124@gmail.com>
2026-03-06 23:31:32 +0700

    Add files via upload

---
+++ Bai9.cs
@@ -0,0 +1,29 @@
+﻿//Kiem tra so nguyen to
+using System;
+
+namespace Bai9
+{
+    class Bai9
+    {
+        static void IsPrime(int n)
+        {
+            if (n < 2)
+            {
+                Console.WriteLine(n +" khong phai so nguyen to");
+            }
+            for (int i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (n % i == 1)
+                {
+                    Console.WriteLine(n +" la so nguyen to");
+                }
+                else Console.WriteLine(n +  " khong phai so nguyen to");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            IsPrime(n);
+        }
+    }
+}
