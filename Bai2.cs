15f50534aaa72aedc9d24b721e7220cd5b0f8c2d
nghailong124-afk <nghailong124@gmail.com>
2026-03-06 21:10:11 +0700

    Add files via upload

---
+++ Bai2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bai2 {
+    class Program {
+        static void Main(string[] args) {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tong a va b la: " + (a + b));
+            Console.WriteLine("Hieu a va b la: " + (a - b));
+            Console.WriteLine("Tich a va b la: " + (a * b));
+            if (b == 0)
+            {
+                Console.WriteLine("Khong the chia");
+            }
+            else Console.WriteLine("Thuong cua a va b la: " + (a / b));
+        }
+    }
+}
