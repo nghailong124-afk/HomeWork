14e65c2bc95f1928ba6afa19b4a84140c30103c8
nghailong124-afk <nghailong124@gmail.com>
2026-03-06 22:28:04 +0700

    Add files via upload

---
+++ Bai6.cs
@@ -0,0 +1,13 @@
+﻿//Dao nguoc chuoi
+using System;
+
+namespace Bai6 {
+    class Program {
+        static void Main(string[] args) {
+            string s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--) {
+                Console.Write(s[i]);
+            }
+        }
+    }
+}
