8ada9f3667f9d61c9ae6a49052db73d45ca69c1a
nghailong124-afk <nghailong124@gmail.com>
2026-03-06 21:23:52 +0700

    Add files via upload

---
+++ Bai3.cs
@@ -0,0 +1,25 @@
+﻿//Kiem tra chan/le va am/duong
+using System;
+
+
+namespace Bai3 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Nhap so nguyen n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 0 && n % 2 == 0)
+            {
+                Console.WriteLine("n la so nguyen duong chan");
+            }
+            else if (n < 0 && n % 2 == 0)
+            {
+                Console.WriteLine("n la so nguyen am chan");
+            }
+            else if (n < 0 && n % 2 == 1)
+            {
+                Console.WriteLine("n la so nguyen am le");
+            }
+            else Console.WriteLine("n la so nguyen duong le");
+        }
+    }
+}
