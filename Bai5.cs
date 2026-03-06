4af2135cb085092b9088d85eb093aa5f1761fa7b
nghailong124-afk <nghailong124@gmail.com>
2026-03-06 22:18:18 +0700

    Add files via upload

---
+++ Bai5.cs
@@ -0,0 +1,22 @@
+﻿//Tinh tong day so 
+using System;
+
+namespace Bai5
+{
+    class Bai5
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Nhap so nguyen duong n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i <= n;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            } Console.WriteLine(sum);
+        }
+    }
+}
