73102029e4c8378d8e7dd982cda14613f95b50ec
nghailong124-afk <nghailong124@gmail.com>
2026-03-06 22:43:34 +0700

    Add files via upload

---
+++ Bai7.cs
@@ -0,0 +1,25 @@
+﻿//Mang va so nguyen
+using System;
+using System.Security.Cryptography;
+
+namespace Bai7
+{
+    class Bai7
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            double answer = 0;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                answer = arr.Sum() / n;
+            }
+            Console.WriteLine("Phan tu lon nhat la: " + arr.Max());
+            Console.WriteLine("Phan tu nho nhat la: " + arr.Min());
+            Console.WriteLine("Tong cac phan tu la: " + arr.Sum());
+            Console.WriteLine("Trung binh cong cac phan tu la: " + answer);
+        }
+    }
+}
